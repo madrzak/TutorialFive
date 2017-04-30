692fa6bebb0ba1021cbefe486156dc827db557d5
Lukasz Madrzak <l.madrzak@gmail.com>
2017-04-30 19:44:25 +0100

    tutorial 5 changes

--- app/src/main/java/com/madrzak/tutorialfour/MainActivity.java
+++ app/src/main/java/com/madrzak/tutorialfour/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private EditText etMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.button);
+        etMessage = (EditText) findViewById(R.id.etMessage);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                intent.putExtra("MESSAGE", etMessage.getText().toString());
+
                 startActivity(intent);
             }
         });
+
 
     }
 }
